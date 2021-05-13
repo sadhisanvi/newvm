5748616c1bcc73889506eb4ea86506a83586e992
Gkasiraju <77190946+Gkasiraju@users.noreply.github.com>
2021-05-13 14:15:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,131 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+
+  name     = "Azurecli33"
+  location = "North Europe"
+  tags = {
+        Environment = "Terraform Getting Started"
+        Team = "DevOps"
+    }
+}
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "myTFVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "westus2"
+    resource_group_name = azurerm_resource_group.rg.name
+}
+# Create public IP
+resource "azurerm_public_ip" "publicip" {
+  name                = "myTFPublicIP"
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  tags = {
+        Environment = "Terraform Getting Started"
+        Team = "DevOps"
+    }
+
+}
+# Create subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "myTFSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "nsg" {
+  name                = "myTFNSG"
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+variable "admin_username" {
+    type = string
+    description = "Administrator user name for virtual machine"
+}
+
+variable "admin_password" {
+    type = string
+    description = "Password must meet Azure complexity requirements"
+}
+ #Create network interface
+resource "azurerm_network_interface" "nic" {
+  name                      = "myNIC"
+  location                  = "westus2"
+  resource_group_name       = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "myNICConfg"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "myTFVM"
+  location              = "westus2"
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_os_disk {
+    name              = "myOsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04.0-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "myTFVM"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
+data "azurerm_public_ip" "ip" {
+  name                = azurerm_public_ip.publicip.name
+  resource_group_name = azurerm_virtual_machine.vm.resource_group_name
+  depends_on          = [azurerm_virtual_machine.vm]
+}
+output "public_ip_address" {
+  value = data.azurerm_public_ip.ip.ip_address
+}
